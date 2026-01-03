364b6250c8a777f471f4c1a2f385ce2af66fb6cf
Muhammed Adnan K <73103641+muhammedadnank@users.noreply.github.com>
2026-01-03 21:58:04 +0530

    Remove security warning comment for secret key
    Removed comment about keeping the secret key secret.

--- todo_project/settings.py
+++ todo_project/settings.py
@@ -2,7 +2,6 @@ from pathlib import Path
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-# SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'SECRET_KEY'
 
 # SECURITY WARNING: don't run with debug turned on in production!
