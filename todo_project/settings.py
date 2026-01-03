9b21c47ca13948f3b053bc5747c7afc5894bedbd
Muhammed Adnan K <73103641+muhammedadnank@users.noreply.github.com>
2026-01-03 22:01:36 +0530

    Clean up settings.py by removing comments
    Removed commented-out password validation section.

--- todo_project/settings.py
+++ todo_project/settings.py
@@ -53,10 +53,6 @@ DATABASES = {
     }
 }
 
-
-# Password validation
-# https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
-
 AUTH_PASSWORD_VALIDATORS = [
     {
         'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
