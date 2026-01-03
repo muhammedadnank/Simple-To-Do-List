34b0428ade4b3ac0a64218e86ee10c404b98f50e
Muhammed Adnan K <73103641+muhammedadnank@users.noreply.github.com>
2026-01-03 22:02:49 +0530

    Update settings.py

--- todo_project/settings.py
+++ todo_project/settings.py
@@ -76,7 +76,6 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
-# Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
